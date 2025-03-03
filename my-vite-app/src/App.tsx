5e944ffbef8cb827f50be5d118714bd8feed00d1
FossXpert <FossXpert@outlook.com>
2025-03-03 15:41:21 +0530

    refactor: update App component layout and remove unnecessary container div

--- my-vite-app/src/App.tsx
+++ my-vite-app/src/App.tsx
@@ -3,12 +3,12 @@ import { fileDownload } from './download'
 
 function App() {
   return (
-    <div style={{display:'flex', justifyContent:'center', alignItems:'center', height:'100vh',backgroundColor:'#000'}}>
+    <>
       <p>Click here to download the file</p>
       <button 
       style={{height:'auto', background:"black", color:'white'}}
       onClick={()=>fileDownload()}>Download</button>
-    </div>
+    </>
   )
 }
 
