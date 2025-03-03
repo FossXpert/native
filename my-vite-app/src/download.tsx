b2a88700134cea626a167e2030f8730c72030bfb
FossXpert <FossXpert@outlook.com>
2025-03-04 00:58:05 +0530

    hh

--- my-vite-app/src/download.tsx
+++ my-vite-app/src/download.tsx
@@ -81,23 +81,7 @@ export const fileDownload = async () => {
   }
 
 }
-const base64ToBlob = (base64: string, contentType = "application/pdf") => {
-  const byteCharacters = atob(base64.split(",")[1]); // Remove `data:application/pdf;base64,`
-  const byteArrays = [];
 
-  for (let i = 0; i < byteCharacters.length; i += 512) {
-    const slice = byteCharacters.slice(i, i + 512);
-    const byteNumbers = new Array(slice.length);
-
-    for (let j = 0; j < slice.length; j++) {
-      byteNumbers[j] = slice.charCodeAt(j);
-    }
-
-    byteArrays.push(new Uint8Array(byteNumbers));
-  }
-
-  return new Blob(byteArrays, { type: contentType });
-};
 
 
 // Helper function to save Blob as file (Without Opening)
